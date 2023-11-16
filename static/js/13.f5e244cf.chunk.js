b42558800bf6a646b811c367ed234614d618e602
spennyp <hello@spencerperkins.me>
2023-11-15 17:19:44 -0800

    Updates

---
+++ static/js/13.f5e244cf.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@nouns/webapp"]=this["webpackJsonp@nouns/webapp"]||[]).push([[13],{1546:function(p,n){}}]);
+//# sourceMappingURL=13.f5e244cf.chunk.js.map
---
+++ static/js/14.549a9ea5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@nouns/webapp"]=this["webpackJsonp@nouns/webapp"]||[]).push([[14],{1474:function(p,n){}}]);
+//# sourceMappingURL=14.549a9ea5.chunk.js.map
